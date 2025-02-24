f6b3b501446b068dcadd15e04f5e224fb9a92aa7
azi-prac <azimemory@gmail.com>
2025-02-24 16:02:28 +0900

    lec: validator

--- src/main/java/com/mc/basic/module/form/FormController.java
+++ src/main/java/com/mc/basic/module/form/FormController.java
@@ -2,11 +2,14 @@ package com.mc.basic.module.form;
 
 import com.mc.basic.module.form.request.FormRequest;
 import com.mc.basic.module.form.response.FormResponse;
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
@@ -30,6 +33,30 @@ public class FormController {
         log.info("model : {}", model);
         return "spring/example";
     }
+
+    @PostMapping
+    public String postForm(
+            @Valid
+            FormRequest request, // @ModelAttribute 암묵적 바인드
+            BindingResult bindingResult,
+            Model model
+    ){
+        log.info("form : {}", request);
+        log.info("bindingResult : {}", bindingResult);
+
+        if(bindingResult.hasErrors()) {
+            return "spring/example";
+        }
+
+        FormResponse response = generateFormResponse(request);
+        model.addAttribute("response", response);
+
+        return "spring/example";
+    }
+
+
+
+
 
     private FormResponse generateFormResponse(FormRequest request) {
         return new FormResponse(request.id(), request.email(), request.name(), LocalDateTime.now());
--- src/main/java/com/mc/basic/module/form/request/FormRequest.java
+++ src/main/java/com/mc/basic/module/form/request/FormRequest.java
@@ -1,14 +1,21 @@
 package com.mc.basic.module.form.request;
 
+import jakarta.validation.constraints.*;
+import org.hibernate.validator.constraints.Length;
+
 import java.time.LocalDateTime;
 
 // record
 // 1. immutable
 // 2. getter를 속성명으로 자동생성
 public record FormRequest(
+        @NotNull
         Integer id,
+        @Email
         String email,
+        @NotEmpty
         String name,
+        @NotNull
         LocalDateTime createdAt
 ) {
 
